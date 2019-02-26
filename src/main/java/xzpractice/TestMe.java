0e8c0f79b8fdf081eef47e60fc737049f17425a6
sandeepnb <sandeepsingh.nb@hotmail.com>
2019-02-26 14:39:30 -0500

    Second Commit

--- src/main/java/xzpractice/TestMe.java
+++ src/main/java/xzpractice/TestMe.java
@@ -1,9 +1,29 @@
 package xzpractice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TestMe {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	public static WebDriver driver;
+	
+		@Test
+		public static void testCase1()
+		{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\GURANSH\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://facebook.com");
+		driver.manage().window().maximize();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
 
 	}
 
